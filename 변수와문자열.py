99f415e18e52fe819992effa3bde5114dc3a870e
NICEBEOM <itbeomhee@nate.com>
2021-11-09 12:29:04 +0900

    1109

---
+++ 변수와문자열.py
@@ -0,0 +1,34 @@
+# # 여러개의 변수를 동시를 초기화
+a, b, c = 1, 2, 3
+print(a)
+print(b)
+print(c)
+
+# # 예제 2~5풀기
+# 예제 2
+
+x = 10
+print(x)
+
+# 예제 3
+x, y, z = 1, 2, 3
+
+print(x)
+print(y)
+print(z)
+
+# 예제 4
+x = 1
+y = 1.1
+z = "문자열"
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# 예제 5
+a = input("a:"+'')
+b = input("b:"+'')
+
+print("a:"+(b))
+print("b:"+(a))
